96b6ef5c0320c569658afa94cc5e37e69c41a6ec
Eduardo Ciconha <131923066+ciconha@users.noreply.github.com>
2025-06-04 21:19:03 -0300

    Update data.py

--- backend/data.py
+++ backend/data.py
@@ -16,16 +16,18 @@ def locais_seguro(chamas):
     return [local for local in locais if local not in chamas]
 
 def gerar_grafo():
+    locais_em_chamas = gerar_chamas()  
+
     graph = {local: {} for local in locais}
     for i in range(len(locais)):
         vizinhos = random.sample(locais, k=random.randint(1, len(locais)//2))  
         for vizinho in vizinhos:
             if vizinho != locais[i]:
-                peso = random.randint(1, 11) if vizinho not in gerar_chamas() else random.randint(20, 60)  
+                peso = random.randint(1, 11) if vizinho not in locais_em_chamas else random.randint(20, 60)  
                 graph[locais[i]][vizinho] = peso
                 graph[vizinho][locais[i]] = peso  
     return graph
 
 graph = gerar_grafo()
 
-print(" Grafo de conexões:", json.dumps(graph, indent=6))
+print("📌 Grafo de conexões:", json.dumps(graph, indent=11))
